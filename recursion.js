df53d4167370b299899a1cae2d0e0b8de49ea276
Sager-Alarifi-GA <59921473+Sager-Alarifi-GA@users.noreply.github.com>
2020-02-12 09:24:38 +0300

    update the formatting in recursion.js

--- recursion.js
+++ recursion.js
@@ -3,92 +3,83 @@
 // All functions must use recursion
 
 
-const findMax = function(arr, index=1, max){
-    // if there's no max yet, set it to the first element
-    if(max == null && arr.length){
-        max = arr[0];
+const findMax = function(arr, index = 1, max) {
+  // if there's no max yet, set it to the first element
+  if (max == null && arr.length) {
+    max = arr[0];
+  }
+  // the base case is when all indices have been searched
+  if (index == arr.length) {
+    return max;
+  } else {
+    if (arr[index] > max) {
+      max = arr[index];
     }
-    // the base case is when all indices have been searched
-    if(index == arr.length){
-        return max;
-    }else{
-        if(arr[index] >  max){
-            max = arr[index];
-        }
-        return findMax(arr, index+1, max);
-    }
-}
+    return findMax(arr, index + 1, max);
+  }
+};
 
 
-const factorial = function(num){
-    if (num == 1 || num == 0){
-        return 1;
+const factorial = function(num) {
+  if (num == 1 || num == 0) {
+    return 1;
+  } else {
+    return num * factorial(num - 1);
+  }
+};
+
+
+const fibonacci = function(num) {
+  // Exception cases in fibonacci
+  if (num < 0) {
+    return "fibonacci only accepts zero and higher integers";
+  } else if (num == 0) {
+    return 0;
+  } else if (num == 1 || num == 2) {
+    return 1;
+  }
+
+  return fibonacci(num - 1) + fibonacci(num - 2);
+};
+
+
+const coinFlips = function(num) {
+  const results = [];
+  console.log("Result: ", results);
+  function rCoinFlips(stem = "") {
+    console.log("Stem: ", stem);
+    if (stem.length === num) {
+      console.log("Results Stem before push: ", results, stem);
+      results.push(stem);
+      console.log("Results Stem 2 after push: ", results, stem);
     } else {
-        return  num * factorial(num - 1);
+      rCoinFlips(stem + "H");
+      rCoinFlips(stem + "T");
     }
-}
+  }
+  rCoinFlips();
+  return results;
+};
 
 
-
-const fibonacci = function(num){
-
-    // Exception cases in fibonacci
-    if(num<0){
-    return "fibonacci only accepts zero and higher integers"
+const letterCombinations = function(arr) {
+  const results = [];
+  function rLetterCombinations(stem, remainder) {
+    console.log("Stem: ", stem);
+    console.log("Remainder: ", remainder);
+    if (stem.length === arr.length) {
+      results.push(stem);
+      return;
+    }
+    if (stem.length > 0) {
+      results.push(stem);
     }
 
-    else if(num == 0){
-    return 0
+    for (let i = 0; i < remainder.length; i++) {
+      const newRemainder = remainder.slice(0, i).concat(remainder.slice(i + 1));
+      rLetterCombinations(stem + remainder[i], newRemainder);
     }
-
-    else if(num == 1 || num == 2){ 
-        return 1;
-    }
-
-    return fibonacci(num-1) + fibonacci(num-2);
-}
-
-
-
-const coinFlips = function(num){
-    const results = [];
-    console.log('Result: ',results);
-    function rCoinFlips(stem=""){
-        console.log('Stem: ', stem);
-        if(stem.length ===  num){
-            console.log('Results Stem before push: ', results, stem);
-            results.push(stem);
-            console.log('Results Stem 2 after push: ', results, stem);
-        } else {
-
-            rCoinFlips(stem+"H");
-            rCoinFlips(stem+"T");
-        }
-    }
-    rCoinFlips();
-    return results;
-}
-
-
-
-const letterCombinations = function(arr){
-    const results = [];
-    function rLetterCombinations(stem, remainder){
-        console.log('Stem: ', stem);
-        console.log('Remainder: ', remainder);
-        if(stem.length === arr.length){
-            results.push(stem);
-            return;
-        }
-        if(stem.length > 0){
-            results.push(stem);
-        }
-
-        for(let i = 0; i < remainder.length; i++){
-            const newRemainder  = remainder.slice(0,i).concat(remainder.slice(i+1));
-            rLetterCombinations(stem+remainder[i], newRemainder)
-        }
-    };
-    rLetterCombinations("", arr);
-    return results;
-}
+  }
+  rLetterCombinations("", arr);
+  return results;
+};
